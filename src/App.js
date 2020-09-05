283c3e844f3bfabfbbf631074d2a36fdb78b8a8f
mazuh <marcell-mz@hotmail.com>
2020-09-05 18:54:47 -0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#">Moneycog</Navbar.Brand>
+        <Navbar.Brand href="/">Moneycog</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#">Início</Nav.Link>
           <Nav.Link href="#">Plano mensal</Nav.Link>
@@ -17,6 +18,15 @@ export default function App() {
           <Nav.Link href="#">Categorias</Nav.Link>
         </Nav>
       </Navbar>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
+const Home = () => (
+  <h1>Hello there.</h1>
+);

51b07a9fff61567df381a8a64b30e591dc99f7dc
mazuh <marcell-mz@hotmail.com>
2020-09-05 18:34:18 -0300

    add react-bootstrap, and a initial main menu

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">Moneycog</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#">Início</Nav.Link>
+          <Nav.Link href="#">Plano mensal</Nav.Link>
+          <Nav.Link href="#">Plano semanal</Nav.Link>
+          <Nav.Link href="#">Transações reais</Nav.Link>
+          <Nav.Link href="#">Categorias</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }

ffe6da4f8eb96b562440d23efafac2680beacb6f
mazuh <marcell-mz@hotmail.com>
2020-12-27 12:27:05 -0300

    loading is a little centered with a spinner

--- src/features/loading/LoadingContainer.js
+++ src/features/loading/LoadingContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Spinner from "react-bootstrap/Spinner";
 import Container from "react-bootstrap/Container";
 
 export default function LoadingContainer() {
   return (
-    <Container>
+    <Container className="d-flex justify-content-center align-items-center pb-5 mb-5 pt-5 mt-5">
+      <Spinner animation="border" variant="info" role="status" className="mr-3" />
       Carregando dados...
     </Container>
   );

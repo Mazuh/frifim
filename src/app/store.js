1d4fb7626128b7eaf8cfd7fdbbaf2ab15395bb8a
mazuh <marcell-mz@hotmail.com>
2021-01-02 14:59:54 -0300

    persist auth state

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import localPersitence from "redux-persist/lib/storage";
 import authReducer from '../features/auth/authDuck';
 import categoriesReducer from '../features/categories/categoriesDuck';
 import monthlyBudgetReducer from '../features/monthly-budget/monthlyBudgetDuck';
 import weeklyBudgetReducer from '../features/weekly-budget/weeklyBudgetDuck';
 import transactionsReducer from '../features/transactions/transactionsDuck';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistReducer({ key: 'auth', storage: localPersitence }, authReducer),
     categories: categoriesReducer,
     monthlyBudget: monthlyBudgetReducer,
     weeklyBudget: weeklyBudgetReducer,
     transactions: transactionsReducer,
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REGISTER, PAUSE, REHYDRATE, PERSIST, PURGE],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
+
+export default store;

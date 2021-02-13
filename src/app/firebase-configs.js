3c0099fbc46a4d10e9e330abc4599a1d55898b19
mazuh <marcell-mz@hotmail.com>
2021-02-13 20:50:12 -0300

    importing only the firebase sdk parts really used

--- src/app/firebase-configs.js
+++ src/app/firebase-configs.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional

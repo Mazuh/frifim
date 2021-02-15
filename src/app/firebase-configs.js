8a42d78e3b695ff116aebfc48bc9dc5c5a7f4df9
mazuh <marcell-mz@hotmail.com>
2021-02-15 17:49:44 -0300

    firebase configs are now passed by env data
    the data here about moneycog is no longer valid, of course,
    but in any case this data will probably be available to end users anyway.

--- src/app/firebase-configs.js
+++ src/app/firebase-configs.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = Object.freeze({
-  apiKey: "AIzaSyAghbovdaW07pXPy3DH32-qrYxlm0ZSXrY",
-  authDomain: "moneycog-70638.firebaseapp.com",
-  projectId: "moneycog-70638",
-  storageBucket: "moneycog-70638.appspot.com",
-  messagingSenderId: "104366726847",
-  appId: "1:104366726847:web:f6cc18df837d52e71c700a",
-  measurementId: "G-NE8E0YTGTF"
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 });
 
 // Initialize Firebase

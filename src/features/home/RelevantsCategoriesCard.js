5a65cb3de916ef8ea313ce9f31c9472a79dd0221
Rodrigo Muniz <rcmuniz51@gmail.com>
2021-08-23 22:32:14 -0300

    feat: add show more button

--- src/features/home/RelevantsCategoriesCard.js
+++ src/features/home/RelevantsCategoriesCard.js
@@ -1,36 +1,62 @@
 import Decimal from 'decimal.js';
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
 import { useSelector } from 'react-redux';
 import RelevantsCategoriesChart from './RelevantsCategoriesChart';
 
 export default function RelevantsCategoriesCard({ cardTitle, cardIcon, groupedAmountsByCategory }) {
+  const [showOthers, setShowOthers] = React.useState(false);
   const categories = useSelector((state) => state.categories.items);
+  const sortedAmounts = groupedAmountsByCategory.sort((a, b) =>
+    Decimal(b.amount).minus(a.amount).toFixed(2)
+  );
   return (
     <Card>
-      <Card.Header className="bg-dark text-light">
+      <Card.Header className="bg-dark text-light d-flex justify-content-between">
         <Card.Title as="h2">
           {cardIcon} {cardTitle}
         </Card.Title>
+        {sortedAmounts.length > 3 && (
+          <Button
+            className="text-light"
+            onClick={() => setShowOthers(!showOthers)}
+            variant="link"
+            aria-controls="collapse-list"
+            aria-expanded={showOthers}
+          >
+            {!showOthers ? 'Ver mais' : 'Ver menos'}
+          </Button>
+        )}
       </Card.Header>
       <Card.Body>
-        <ol>
-          {groupedAmountsByCategory
-            .sort((a, b) => Decimal(b.amount).minus(a.amount).toFixed(2))
-            .slice(0, 3)
-            .map((income) => {
-              const currentCategory = categories.find(
-                (category) => category.uuid === income.category
-              );
-              return (
-                <li key={income.category}>
-                  {`${currentCategory ? currentCategory.name : 'Sem categoria'}: R$ ${
-                    income.amount
-                  }`}
-                </li>
-              );
-            })}
+        <ol className="mb-0">
+          {sortedAmounts.slice(0, 3).map((sa) => {
+            const currentCategory = categories.find((category) => category.uuid === sa.category);
+            return (
+              <li key={sa.category}>
+                {`${currentCategory ? currentCategory.name : 'Sem categoria'}: R$ ${sa.amount}`}
+              </li>
+            );
+          })}
         </ol>
+        {sortedAmounts.length > 3 && (
+          <Collapse in={showOthers}>
+            <ol start="4" id="collapse-list">
+              {sortedAmounts.slice(3).map((sa) => {
+                const currentCategory = categories.find(
+                  (category) => category.uuid === sa.category
+                );
+                return (
+                  <li key={sa.category}>
+                    {`${currentCategory ? currentCategory.name : 'Sem categoria'}: R$ ${sa.amount}`}
+                  </li>
+                );
+              })}
+            </ol>
+          </Collapse>
+        )}
         <RelevantsCategoriesChart
           datasetName={cardTitle}
           groupedAmountsByCategory={groupedAmountsByCategory}

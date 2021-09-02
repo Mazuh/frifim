33818b534eea1a0c2b959def08edc19af89fdb3c
mazuh <marcell-mz@hotmail.com>
2021-09-02 04:51:42 -0300

    run prettier, and put most abstract component on top

--- src/features/auth/EmailVerification.js
+++ src/features/auth/EmailVerification.js
@@ -5,14 +5,38 @@ import { sendEmailVerification } from '../../app/firebase-configs';
 import useBasicRequestData from '../../app/useBasicRequestData';
 import { useSelector } from 'react-redux';
 
+export default function EmailVerification() {
+  const { hasSocialMediaAuthType, sendVerificationLink, linkSent, emailVerified, isAuthorized } =
+    useEmailVerification();
+
+  if (!isAuthorized || hasSocialMediaAuthType || emailVerified) {
+    return null;
+  }
+
+  return linkSent ? (
+    <Alert variant="info">
+      Enviamos um link de verificação ao seu e-mail. (Cheque a caixa de spam também!)
+    </Alert>
+  ) : (
+    <Alert variant="warning">
+      Hey, parece que você ainda não verificou seu e-mail:{' '}
+      <Alert.Link href="#" onClick={sendVerificationLink}>
+        clique aqui
+      </Alert.Link>{' '}
+      para receber um novo link de verificação.
+    </Alert>
+  );
+}
+
 export const useEmailVerification = () => {
   const { user } = useBasicRequestData();
-  const isAuthorized = useSelector(state => state.auth.isAuthorized);
+  const isAuthorized = useSelector((state) => state.auth.isAuthorized);
   const [linkSent, setLinkSent] = useState(false);
 
-  const hasSocialMediaAuthType = user
-    && user.providerData
-    && user.providerData.some(({ providerId }) => providerId !== 'password');
+  const hasSocialMediaAuthType =
+    user &&
+    user.providerData &&
+    user.providerData.some(({ providerId }) => providerId !== 'password');
 
   const { emailVerified = false } = user || {};
 
@@ -27,7 +51,7 @@ export const useEmailVerification = () => {
           color: 'red',
           position: 'topCenter',
           timeout: 2000,
-        })
+        });
       });
   };
 
@@ -39,33 +63,3 @@ export const useEmailVerification = () => {
     isAuthorized,
   };
 };
-
-const EmailVerification = () => {
-  const {
-    hasSocialMediaAuthType,
-    sendVerificationLink,
-    linkSent,
-    emailVerified,
-    isAuthorized,
-  } = useEmailVerification();
-
-  if (!isAuthorized || hasSocialMediaAuthType || emailVerified) {
-    return null;
-  }
-
-  return (
-    linkSent ? (
-        <Alert variant="info">
-          Enviamos um link de verificação ao seu e-mail. (Cheque a caixa de spam também!)
-        </Alert>
-      ) : (
-        <Alert variant="warning">
-          Hey, parece que você ainda não verificou seu e-mail: {' '}
-          <Alert.Link href="#" onClick={sendVerificationLink}>clique aqui</Alert.Link> {' '}
-          para receber um novo link de verificação.
-        </Alert>
-      )
-  );
-};
-
-export default EmailVerification;

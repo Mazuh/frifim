4c61cdad4fedf698073b023119b912f7eb5696b0
Rodrigo Muniz <rcmuniz51@gmail.com>
2021-10-19 23:59:00 -0300

    change separator

--- src/utils/monthly-budget-utils.js
+++ src/utils/monthly-budget-utils.js
@@ -2,14 +2,15 @@ export function generateMonthlyBudgetReport(header, incomes, expenses) {
   const incomesData = incomes.map((i) => [i.name, 'Receita', i.amount, i.categoryName]);
   const expensesData = expenses.map((e) => [e.name, 'Despesa', e.amount, e.categoryName]);
 
+  const headerRow = header.join(';');
   const incomesRows = incomesData.length
-    ? `\n${incomesData.map((i) => i.join(',')).join('\n')}`
+    ? `\n${incomesData.map((i) => i.join(';')).join('\n')}`
     : '';
   const expensesRows = expensesData.length
-    ? `\n${expensesData.map((i) => i.join(',')).join('\n')}`
+    ? `\n${expensesData.map((i) => i.join(';')).join('\n')}`
     : '';
 
-  const csvContent = `data:text/csv;charset=utf-8,${header}${incomesRows}${expensesRows}`;
+  const csvContent = `data:text/csv;charset=utf-8,${headerRow}${incomesRows}${expensesRows}`;
 
   return encodeURI(csvContent);
 }

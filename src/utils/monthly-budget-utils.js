352e3df959a175a34f549e088ee4b6af9387fb14
Rodrigo Muniz <rcmuniz51@gmail.com>
2021-10-20 22:54:07 -0300

    set default separator

--- src/utils/monthly-budget-utils.js
+++ src/utils/monthly-budget-utils.js
@@ -2,15 +2,15 @@ export function generateMonthlyBudgetCSV(header, incomes, expenses) {
   const incomesData = incomes.map((i) => [i.name, 'Receita', i.amount, i.categoryName]);
   const expensesData = expenses.map((e) => [e.name, 'Despesa', e.amount, e.categoryName]);
 
-  const headerRow = header.join(';');
+  const headerRow = header.join(',');
   const incomesRows = incomesData.length
-    ? `\n${incomesData.map((i) => i.join(';')).join('\n')}`
+    ? `\n${incomesData.map((i) => i.map((it) => `"${it}"`).join(',')).join('\n')}`
     : '';
   const expensesRows = expensesData.length
-    ? `\n${expensesData.map((i) => i.join(';')).join('\n')}`
+    ? `\n${expensesData.map((i) => i.map((it) => `"${it}"`).join(',')).join('\n')}`
     : '';
 
-  const csvContent = `data:text/csv;charset=utf-8,${headerRow}${incomesRows}${expensesRows}`;
+  const csvContent = `data:text/csv;charset=utf-8,sep=,\n${headerRow}${incomesRows}${expensesRows}`;
 
   return encodeURI(csvContent);
 }

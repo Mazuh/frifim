3c6909a3eca4f4a14646637de3b0b2f07eaf13df
roxdavirox <roxdavirox@gmail.com>
2021-11-28 17:47:49 -0300

    feat: new component with itself business logic

---
+++ src/features/emergency-saving/EmegencySavingIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MdOutlineSavings, MdNotificationImportant } from 'react-icons/md';
+import useEmergencySaving from './useEmergencySaving';
+
+const EmergencySavingIcon = () => {
+	const shouldEmergencySaving = useEmergencySaving();
+	
+	return shouldEmergencySaving
+		? <MdNotificationImportant color="orange" />
+		: <MdOutlineSavings />;
+};
+
+export default EmergencySavingIcon;

a7880a799a7f7bcd40f046f837cd47a4335f17fd
roxdavirox <roxdavirox@gmail.com>
2021-12-01 23:46:20 -0300

    feat: new emergency save simulator component

---
+++ src/features/emergency-saving/EmergencySimulator.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { BsCalculator } from 'react-icons/bs';
+import { MainSection } from '../main-pages/main-pages';
+import NumberFormat from 'react-number-format';
+import useEmergencySimulator from './useEmergencySimulator';
+
+const fields = [
+  {
+    id: 'monthQuantity',
+    title: 'Quantidade de meses necessarios para o fundo de emergência:',
+    preprend: 'n°',
+    decimalScale: 0,
+    legend: (
+      <Form.Group>
+        <Form.Text className="text-muted">
+          Quantidade de meses sugerida padrão é de 3 meses para funcionario publico, 6 para carteira
+          assinada (CLT) e 12 meses para os demais (estágio, informal, PJ etc.)
+        </Form.Text>
+      </Form.Group>
+    ),
+  },
+  {
+    id: 'expenses',
+    title: 'Despesas:',
+    preprend: 'R$',
+    decimalScale: 2,
+    placeholder: 'Ex: 10, 00',
+  },
+  {
+    id: 'recommendedEmergency',
+    title: 'Valor mensal recomendado para guardar no fundo de emergência para atingir o objetivo:',
+    preprend: 'R$',
+    placeholder: 'o valor recomendado é de 10% de suas despesas mensais',
+    decimalScale: 2,
+  },
+  {
+    id: 'previusSavedMoney',
+    title: 'Dinheiro guardado:',
+    preprend: 'R$',
+    placeholder: 'Já possui algum dinheiro guardado?',
+    decimalScale: 2,
+  },
+];
+
+const EmergencySimulator = () => {
+  const { change, objectiveTime, objective, getValue } = useEmergencySimulator(fields);
+
+  return (
+    <MainSection icon={<BsCalculator />} title="Simulação">
+      <Form>
+        {fields.map(({ id, title, preprend, decimalScale, legend, placeholder }) => (
+          <Form.Group className="mb-3">
+            <Form.Label>{title}</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>{preprend}</InputGroup.Text>
+              </InputGroup.Prepend>
+              <NumberFormat
+                name={id}
+                autoComplete="off"
+                placeholder={placeholder}
+                inputMode="decimal"
+                value={getValue(id)}
+                onValueChange={change(id)}
+                displayType={'input'}
+                fixedDecimalScale
+                decimalSeparator={','}
+                thousandSeparator={'.'}
+                decimalScale={decimalScale}
+                className="form-control"
+              />
+            </InputGroup>
+            {legend && legend}
+          </Form.Group>
+        ))}
+
+        <Form.Group>
+          <h6>
+            Valor total do fundo de emergência estimado para atingir o objetivo{' '}
+            {
+              <NumberFormat
+                defaultValue={objective}
+                value={objective}
+                fixedDecimalScale
+                decimalSeparator={','}
+                thousandSeparator={'.'}
+                decimalScale={2}
+                displayType="text"
+              />
+            }
+          </h6>
+          {objectiveTime > 0 && (
+            <h6>
+              Tempo para atingir o objetivo {objectiveTime}
+              {objectiveTime > 1 ? ' meses' : ' mês'}
+            </h6>
+          )}
+        </Form.Group>
+      </Form>
+    </MainSection>
+  );
+};
+
+export default EmergencySimulator;

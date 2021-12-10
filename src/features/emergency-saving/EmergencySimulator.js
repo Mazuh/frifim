1765ff5879356c85a63bcd0e81ce242cf05b66fd
roxdavirox <roxdavirox@gmail.com>
2021-12-10 14:48:07 -0300

    refactor: isolate emergency fields from componentn

--- src/features/emergency-saving/EmergencySimulator.js
+++ src/features/emergency-saving/EmergencySimulator.js
@@ -5,78 +5,44 @@ import { BsCalculator } from 'react-icons/bs';
 import { MainSection } from '../main-pages/main-pages';
 import NumberFormat from 'react-number-format';
 import useEmergencySimulator from './useEmergencySimulator';
-
-const fields = [
-  {
-    id: 'monthQuantity',
-    label: 'Quantidade de meses necessarios para o fundo de emergência:',
-    prepend: 'meses',
-    prependStyle: { display: 'flex', flexDirection: 'row-reverse' },
-    decimalScale: 0,
-    legend: (
-      <Form.Group>
-        <Form.Text className="text-muted">
-          Dica: 3 meses para funcionalismo público, 6 meses para carteira assinada (CLT) e 12 meses
-          para os demais (estágio, informal, PJ etc.).
-        </Form.Text>
-      </Form.Group>
-    ),
-  },
-  {
-    id: 'expenses',
-    label: 'Despesas:',
-    prepend: 'R$',
-    decimalScale: 2,
-    placeholder: 'Ex: 10, 00',
-  },
-  {
-    id: 'recommendedEmergency',
-    label: 'Valor mensal recomendado para guardar no fundo de emergência para atingir o objetivo:',
-    prepend: 'R$',
-    placeholder: 'o valor recomendado é de 10% de suas despesas mensais',
-    decimalScale: 2,
-  },
-  {
-    id: 'previusSavedMoney',
-    label: 'Dinheiro guardado:',
-    prepend: 'R$',
-    placeholder: 'Já possui algum dinheiro guardado?',
-    decimalScale: 2,
-  },
-];
+import { emergencyFields } from './constants';
 
 const EmergencySimulator = () => {
-  const { change, objectiveTime, objective, getValue } = useEmergencySimulator(fields);
+  const { change, objectiveTime, objective, getValue } = useEmergencySimulator(emergencyFields);
 
   return (
     <MainSection icon={<BsCalculator />} title="Simulação">
       <Form>
-        {fields.map(({ id, label, prepend, prependStyle, decimalScale, legend, placeholder }) => (
-          <Form.Group className="mb-3" key={id}>
-            <Form.Label>{label}</Form.Label>
-            <InputGroup style={prependStyle}>
-              <InputGroup.Prepend>
-                <InputGroup.Text>{prepend}</InputGroup.Text>
-              </InputGroup.Prepend>
-              <NumberFormat
-                data-testid={id}
-                name={id}
-                autoComplete="off"
-                placeholder={placeholder}
-                inputMode="decimal"
-                value={getValue(id)}
-                onValueChange={change(id)}
-                displayType={'input'}
-                fixedDecimalScale
-                decimalSeparator={','}
-                thousandSeparator={'.'}
-                decimalScale={decimalScale}
-                className="form-control"
-              />
-            </InputGroup>
-            {legend}
-          </Form.Group>
-        ))}
+        {emergencyFields.map(
+          ({ id, label, prepend, prependStyle, decimalScale, legend, placeholder }) => (
+            <Form.Group className="mb-3" key={id}>
+              <Form.Label>{label}</Form.Label>
+              <InputGroup style={prependStyle}>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>{prepend}</InputGroup.Text>
+                </InputGroup.Prepend>
+                <NumberFormat
+                  data-testid={id}
+                  name={id}
+                  autoComplete="off"
+                  placeholder={placeholder}
+                  inputMode="decimal"
+                  value={getValue(id)}
+                  onValueChange={change(id)}
+                  displayType={'input'}
+                  fixedDecimalScale
+                  decimalSeparator={','}
+                  thousandSeparator={'.'}
+                  decimalScale={decimalScale}
+                  className="form-control"
+                />
+              </InputGroup>
+              <Form.Group>
+                <Form.Text className="text-muted">{legend}</Form.Text>
+              </Form.Group>
+            </Form.Group>
+          )
+        )}
 
         <Form.Group>
           <h6>

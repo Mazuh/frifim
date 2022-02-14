a239db22333a02e90e5f42f2881e23bd26cbf51e
Rodrigo Muniz <rcmuniz51@gmail.com>
2022-02-13 21:38:02 -0300

    create teste for the new messages of the emergency save simulator

--- src/features/emergency-saving/EmergencySimulator.test.js
+++ src/features/emergency-saving/EmergencySimulator.test.js
@@ -118,4 +118,85 @@ describe('emergency saving simulator', () => {
       'Você precisa de uma reserva total de R$ 4.200,00.Ao fim de 18 meses, você terá pelo menos R$ 4.321,00 reservados para emergência.'
     );
   });
+
+  it('asks for required data when there are not default values', () => {
+    const store = makeConfiguredStore();
+
+    const container = render(
+      <Provider store={store}>
+        <EmergencySimulator />
+      </Provider>
+    );
+
+    expect(container.getByText(/Por favor, informe/, { selector: 'span' }).textContent).toBe(
+      'Por favor, informe pelo menos os valores de meses para proteger, despesas fixas por mês para obter um resultado.'
+    );
+  });
+
+  it('shows message about the previously saved money is already enough', () => {
+    const store = makeConfiguredStore();
+    store.dispatch(
+      monthlyBudgetPlainActions.setRead(null, [
+        {
+          rememberOnDashboard: true,
+          month: new Date().getMonth(),
+          amount: '2400.00',
+          category: '',
+          project: 'KO4nF9EDng0XEYI1KeOT',
+          year: new Date().getFullYear(),
+          name: 'Salário',
+          type: 'income',
+          userUid: 'NcxuU13pLHMsTPqoHSKYSL03PRb2',
+          uuid: 'VQlo3e4frBJbUwpF4Fys',
+        },
+        {
+          project: 'KO4nF9EDng0XEYI1KeOT',
+          rememberOnDashboard: true,
+          category: '',
+          month: new Date().getMonth(),
+          amount: '200.00',
+          name: 'Energia',
+          year: new Date().getFullYear(),
+          userUid: 'NcxuU13pLHMsTPqoHSKYSL03PRb2',
+          type: 'expense',
+          uuid: 'WrEFWVWN0MtIxmbDavaH',
+        },
+        {
+          project: 'KO4nF9EDng0XEYI1KeOT',
+          rememberOnDashboard: true,
+          category: '',
+          month: new Date().getMonth(),
+          amount: '500.00',
+          name: 'Feira',
+          year: new Date().getFullYear(),
+          userUid: 'NcxuU13pLHMsTPqoHSKYSL03PRb2',
+          type: 'expense',
+          uuid: 'WrEFWVWN0MtfffffIxmbDavaH',
+        },
+      ])
+    );
+
+    const container = render(
+      <Provider store={store}>
+        <EmergencySimulator />
+      </Provider>
+    );
+
+    const monthQuantityInput = container.getByLabelText('Meses para proteger:');
+    fireEvent.change(monthQuantityInput, { target: { value: '3' } });
+
+    expect(container.getByLabelText('Despesas fixas por mês:').value).toBe('700,00');
+
+    fireEvent.change(container.getByLabelText('Quanto guardará todo mês:'), {
+      target: { value: '240,00' },
+    });
+
+    fireEvent.change(container.getByLabelText('Quantia já guardada:'), {
+      target: { value: '2500,00' },
+    });
+
+    expect(container.getByText(/A quantia já guardada/, { selector: 'span' }).textContent).toBe(
+      'A quantia já guardada é suficiente para os meses que você deseja proteger.'
+    );
+  });
 });

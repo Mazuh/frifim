1968eff6ea57dcc86ce712224b51139726e45419
Rodrigo Muniz <rcmuniz51@gmail.com>
2022-03-14 21:59:40 -0300

    retrieve shared monthly budgets

--- src/features/monthly-budget/monthlyBudgetDuck.js
+++ src/features/monthly-budget/monthlyBudgetDuck.js
@@ -9,18 +9,27 @@ const monthlyBudgetResource = makeReduxAssets({
   idKey: 'uuid',
   makeMessageText: makeResourceMessageTextFn('planejamento', 'planejamentos'),
   gateway: {
-    fetchMany: (uuid, basicData) =>
-      client
-        .query(basicData)
-        .where('year', '==', basicData.year)
-        .where('month', '==', basicData.month)
-        .get()
-        .then(parseQuerySnapshot),
+    fetchMany: (uuid, basicData) => retrieveMonthlyBudgets(basicData),
     create: (budget, basicData) => client.create(basicData, budget),
     update: (uuid, budget, basicData) => client.update(basicData, budget),
     delete: (uuid) => client.delete(uuid),
   },
 });
+
+const retrieveMonthlyBudgets = (basicData) =>
+  basicData.project.guestsUids.length > 0
+    ? client
+        .querySharedData(basicData)
+        .where('year', '==', basicData.year)
+        .where('month', '==', basicData.month)
+        .get()
+        .then(parseQuerySnapshot)
+    : client
+        .query(basicData)
+        .where('year', '==', basicData.year)
+        .where('month', '==', basicData.month)
+        .get()
+        .then(parseQuerySnapshot);
 
 export const { actionThunks: monthlyBudgetActions, plainActions: monthlyBudgetPlainActions } =
   monthlyBudgetResource;

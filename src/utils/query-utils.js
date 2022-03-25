b9a83e0088a61d449132b7dc6696b82aebbcf92f
Rodrigo Muniz <rcmuniz51@gmail.com>
2022-03-24 22:19:03 -0300

    make update data button update projects

--- src/utils/query-utils.js
+++ src/utils/query-utils.js
@@ -1,7 +1,16 @@
 import get from 'lodash.get';
+import { retrieveProjects } from './project-utils';
 
 export const queryByClient = (client, basicData) => {
   return get(basicData, 'project.guestsEmails', null)
     ? client.querySharedData(basicData)
     : client.query(basicData);
 };
+
+export async function verifyBasicData(basicData) {
+  const projects = await retrieveProjects(basicData);
+
+  const currentProjectExist = projects.some((project) => project.uuid === basicData.project.uuid);
+
+  return currentProjectExist ? basicData : { ...basicData, project: projects[0] };
+}
